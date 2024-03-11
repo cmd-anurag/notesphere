5e811243df9471c52ef508f1b79e7bc6b541f648
cmd-anurag <153152868+cmd-anurag@users.noreply.github.com>
2024-03-11 18:08:50 +0530

    db fixes

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://localhost:27017/notesphere"
+const mongoURI = `mongodb+srv://capedcrusader2120:${process.env.password}@notesphere.jhm4ly2.mongodb.net/`
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI).then(()=>console.log("Connected to MongoDB")).catch((e)=>console.log(e.message))
